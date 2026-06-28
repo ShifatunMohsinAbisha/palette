a28cb72090574a088ffc140c0e8e35c8c27b072b
ShifatunMohsinAbisha <abishashifatun@gmail.com>
2026-06-28 20:03:52 +0600

    feat: database setup and tables created

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,12 +1,11 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+﻿from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy.orm import declarative_base
 from datetime import datetime
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "users"
-
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
